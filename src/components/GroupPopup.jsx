3fc963c649bffc0abcecf31255aadeb3820f0e73
yogeshkr2002 <yogeshkumarsharma689@gmail.com>
2024-10-05 18:03:59 +0530

    Some changes

--- src/components/GroupPopup.jsx
+++ src/components/GroupPopup.jsx
@@ -14,16 +14,21 @@ const GroupPopup = ({ addGroup, setShowPopup }) => {
   return (
     <div className="group-popup">
       <div className="popup-content">
-        <h3>Create Group</h3>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={groupName}
-            onChange={(e) => setGroupName(e.target.value)}
-            placeholder="Group Name"
-            required
-          />
-          <button type="submit">Create</button>
+        <h3 className="popupHeading">Create New Group</h3>
+        <form onSubmit={handleSubmit} className="popupForm">
+          <div className="one">
+            <label className="inputLabel">Group Name</label>
+            <input
+              type="text"
+              value={groupName}
+              onChange={(e) => setGroupName(e.target.value)}
+              placeholder="Enter group name"
+              required
+            />
+          </div>
+          <div className="two">
+            <button type="submit">Create</button>
+          </div>
         </form>
         <button onClick={() => setShowPopup(false)}>Close</button>
       </div>
